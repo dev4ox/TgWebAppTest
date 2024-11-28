5f5f35ce7290e84d193fe7bf89b1bd79e5eaf600
dev4ox <dev4ox@gmail.com>
2024-11-28 16:05:54 +0300

    init

--- tg_module/tg_module/urls.py
+++ tg_module/tg_module/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('webapp.urls')),
+    path('webapp/admin/', admin.site.urls),
+    path('webapp', include('webapp.urls')),
 ]
 
