e13ba0b2dd57970543e0dec08a58d5625d3b21b5
dev4ox <dev4ox@gmail.com>
2024-11-28 15:57:32 +0300

    init

--- tg_module/tg_module/settings.py
+++ tg_module/tg_module/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = secret.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = secret.DEBUG
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = secret.ALLOWED_HOSTS
 
 
 # Application definition
